41d56acee3b07311b27f4efdaeef19d77e25fc5b
Marcus Schmidt <MarSchmidt@intershop.com>
2020-04-15 15:01:05 +0200

    added master file

--- conf.py
+++ conf.py
@@ -24,6 +24,7 @@ author = 'marcus'
 # The full version, including alpha/beta/rc tags
 release = '1'
 
+master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
 
